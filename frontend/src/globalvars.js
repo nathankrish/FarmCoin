4749b6a9faa640c9ac4c90f1e3bc85b0f7e5ad18
Krish Nathan <nathan.krish.edu@gmail.com>
2021-07-12 12:52:49 -0400

    change to https

--- frontend/src/globalvars.js
+++ frontend/src/globalvars.js
@@ -1,3 +1,3 @@
 // const backendLink = 'http://localhost:5000';
-const backendLink = 'http://enigmatic-bayou-36246.herokuapp.com';
+const backendLink = 'https://enigmatic-bayou-36246.herokuapp.com';
 export {backendLink};
