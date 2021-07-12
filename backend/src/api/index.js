c00e53e49db50dabb98f98077a1a3226a615f38f
Krish Nathan <nathan.krish.edu@gmail.com>
2021-07-12 12:32:57 -0400

    heroku backend changes

--- backend/src/api/index.js
+++ backend/src/api/index.js
@@ -12,7 +12,7 @@ const mongoConnString = `mongodb+srv://${userName}:${password}@cluster0.t8rmg.mo
 await mongoose.connect(mongoConnString, {useNewUrlParser: true, useUnifiedTopology: true});
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000
 app.use(express.json());
 app.use(cors())
 
